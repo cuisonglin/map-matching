88c349e6790f2e4437ab4a1af6436dcce8d3cf59
DengrongGuan12 <1031212913@qq.com>
2015-08-14 14:59:21 +0800

    add OsmRelation.java

---
+++ src/main/java/com/sap/nic/traffic/osm/reader/OSMRelation.java
@@ -0,0 +1,29 @@
+package com.sap.nic.traffic.osm.reader;
+
+import java.util.ArrayList;
+
+public class OSMRelation extends OSMElement{
+	private ArrayList<Member> members = new ArrayList<Member>();
+	public OSMRelation(long id){
+		super(id);
+	}
+	public void addMember(char type, long ref, String role){
+		members.add(new Member(type, ref, role));
+	}
+	
+	public class Member{
+		public static final char TYPE_WAY = 'w';
+		public static final char TYPE_NODE = 'n';
+		private char type;
+		private long ref;
+		private String role;
+		public Member(char type,long ref, String role){
+			this.type = type;
+			this.ref = ref;
+			this.role = role;
+		}
+		
+		
+	}
+
+}
